4bb0a27ae9c64799f60533b369f5998d3d74402b
nashs789 <bbock1214@gmail.com>
2025-10-21 02:16:14 +0900

    Initial commit

---
+++ src/main/java/com/nashs/daily_log/global/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.nashs.daily_log.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/").permitAll()
+                    .anyRequest()
+                    .permitAll() // ← 임시 전체 허용 (SNS 로그인 붙이기 전)
+            );
+        return http.build();
+    }
+}
