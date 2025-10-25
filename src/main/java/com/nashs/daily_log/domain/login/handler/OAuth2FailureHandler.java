1d8ff304974c19fd62bcaebe97852eebc852facc
nashs789 <bbock1214@gmail.com>
2025-10-25 15:32:42 +0900

    🧹Chore: callback 이 제대로 안오는 이유 식별 확인

--- src/main/java/com/nashs/daily_log/domain/login/handler/OAuth2FailureHandler.java
+++ src/main/java/com/nashs/daily_log/domain/login/handler/OAuth2FailureHandler.java
@@ -2,6 +2,7 @@ package com.nashs.daily_log.domain.login.handler;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -11,6 +12,7 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
+@Slf4j
 @Component
 public class OAuth2FailureHandler implements AuthenticationFailureHandler {
     @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
@@ -18,6 +20,7 @@ public class OAuth2FailureHandler implements AuthenticationFailureHandler {
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest req, HttpServletResponse res, AuthenticationException ex) throws IOException {
+        log.info("callback = {}", callback);
         if (callback != null && !callback.isBlank()) {
             String loc = String.format("%s?error=%s", callback, URLEncoder.encode(ex.getMessage(), StandardCharsets.UTF_8));
             res.setStatus(302);
