5880002d543f0ccaeff4cb1a85106df1f091a44d
nashs789 <bbock1214@gmail.com>
2025-11-02 03:50:44 +0900

    ✨Feat & ♻️Refact: 템플릿 조회 리팩토링 및 템플릿 수정 추가
    - 스크립트 코드 리팩토링
    - 서버 구조 재구성
    - 템플릿 수정 기능 추가

---
+++ src/main/java/com/nashs/daily_log/infra/common/exception/InfraException.java
@@ -0,0 +1,15 @@
+package com.nashs.daily_log.infra.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class InfraException extends RuntimeException {
+
+    private HttpStatus status;
+
+    public InfraException(final HttpStatus status, final String msg) {
+        super(msg);
+        this.status = status;
+    }
+}
